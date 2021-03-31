26c0c283dda0397b5be5c5e4217c151b1eb35e1d
Reinhold Kaiser <kaiser@itp.uni-frankfurt.de>
2021-04-01 00:36:44 +0200

    feat: basic initialize_tables_method

--- tests/unit/test_database_class.py
+++ tests/unit/test_database_class.py
@@ -7,6 +7,7 @@ class TestDatabaseMethods:
     def test_init_method(self, tmp_path):
         database = Database(tmp_path / "test_database.db")
         assert isinstance(database.connection, sql.Connection)
+        assert isinstance(database.cursor, sql.Cursor)
         assert database.filename == tmp_path / "test_database.db"
         assert database.connection
 
@@ -14,3 +15,12 @@ class TestDatabaseMethods:
         edf = empty_database_fixture
         edf.close_connection()
         assert not edf.open_connection
+
+    def test_initialize_tables(self, empty_database_fixture):
+        edf = empty_database_fixture
+        edf.initialize_tables()
+        edf.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = edf.cursor.fetchall()
+        for table in [("exercises",), ("workouts",)]:
+            assert table in tables
+
--- workouts_tracking/database.py
+++ workouts_tracking/database.py
@@ -11,9 +11,22 @@ class Database:
     def __init__(self, filename):
         self.filename = filename
         self.connection = sql.connect(self.filename)
+        self.cursor = self.connection.cursor()
         self.open_connection = True
 
     def close_connection(self):
         """Closes the connection in self.connection."""
         self.connection.close()
         self.open_connection = False
+
+    def initialize_tables(self):
+        with self.connection as con:
+            con.execute("CREATE TABLE exercises "
+                        "(id int,"
+                        "name text,"
+                        "category text,"
+                        "muscles_groups text,"
+                        "difficulty text);")
+            con.execute("CREATE TABLE workouts "
+                        "(id int,"
+                        "date int)")
