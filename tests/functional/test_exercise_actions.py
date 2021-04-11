12b35b9cc73242d75408e085e3ea1667281dcf7d
Reinhold Kaiser <kaiser@itp.uni-frankfurt.de>
2021-04-11 23:21:00 +0200

    test: added FT for adding new basic exercise

--- tests/functional/test_exercise_actions.py
+++ tests/functional/test_exercise_actions.py
@@ -6,3 +6,15 @@ class TestEmptyDatabase:
         gef = groupbox_exercise_fixture
         gef.button_new.click()
         assert mwf.window_new_exercise.isVisible()
+
+    def test_added_new_basic_exercise(self, main_window_fixture, database_filename_fixture):
+        mwf = main_window_fixture
+        mwf.new_database(database_filename_fixture)
+        wne = mwf.window_new_exercise
+        wne.line_edit_name.setText("Test Exercise")
+        wne.button_add.click()
+        tables = mwf.database.get_table_names()
+        exercises = mwf.database.get_exercise_names()
+        assert "Test Exercise" in exercises
+        exercise_id = mwf.database.get_exercise_id("Test Exercise")
+        assert f"exercise_{exercise_id}" in tables
