b17f5b0b79983b583ab2c19bc0134a5847e02228
Reinhold Kaiser <kaiser@itp.uni-frankfurt.de>
2021-08-29 11:22:28 +0200

    feat: CurrentWorkout class

--- tests/unit/test_workout_class.py
+++ tests/unit/test_workout_class.py
@@ -1,6 +1,9 @@
 import datetime as dt
 
-from workouts_tracking.workout import Workout
+import pytest
+
+from workouts_tracking.workout import Workout, CurrentWorkout
+from workouts_tracking.errors import ProcedureError
 
 
 class TestWorkoutAttributes:
@@ -13,3 +16,31 @@ class TestWorkoutAttributes:
         assert w.start_time.isoformat() == "12:10:00"
         assert w.end_time.isoformat() == "12:45:00"
         assert w.comment == "Test..."
+
+
+class TestCurrentWorkout:
+    def test_current_workout_attributes(self):
+        cw = CurrentWorkout()
+        assert cw.date is None
+        assert cw.start_time is None
+        assert cw.end_time is None
+        assert cw.comment is None
+        assert cw.open is False
+
+    def test_start_workout(self):
+        cw = CurrentWorkout()
+        current_datetime = dt.datetime.now()
+        date = current_datetime.date()
+        start_time1 = current_datetime.time()
+        cw.start_workout()
+        start_time2 = dt.datetime.now().time()
+        assert date == cw.date
+        assert start_time1 < cw.start_time < start_time2
+        assert cw.open
+        with pytest.raises(ProcedureError):
+            cw.start_workout()
+
+    def test_end_workout(self):
+        cw = CurrentWorkout()
+        with pytest.raises(ProcedureError):
+            cw.end_workout()
--- workouts_tracking/workout.py
+++ workouts_tracking/workout.py
@@ -1,5 +1,7 @@
 import datetime as dt
 
+from workouts_tracking.errors import ProcedureError
+
 
 class Workout:
     def __init__(self, date: dt.date, start_time: dt.time, end_time: dt.time,
@@ -8,3 +10,24 @@ class Workout:
         self.start_time = start_time
         self.end_time = end_time
         self.comment = comment
+
+
+class CurrentWorkout:
+    def __init__(self):
+        self.date = None
+        self.start_time = None
+        self.end_time = None
+        self.comment = None
+        self.open = False
+
+    def start_workout(self):
+        if self.open:
+            raise ProcedureError("The CurrenWorkout has already been started. Cannot start again.")
+        self.date = dt.date.today()
+        self.start_time = dt.datetime.now().time()
+        self.open = True
+
+    def end_workout(self):
+        if not self.open:
+            raise ProcedureError("The CurrentWorkout has not yet been started. Cannot end the"
+                                 " workout.")
